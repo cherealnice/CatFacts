d083c7da352d2ca1beff6ddfedc3d27da970b703
Dan Cherouny <dcherouny@gmail.com>
2016-01-30 09:21:11 -0500

    Rename server file

--- server.js
+++ server.js
@@ -1,19 +1,30 @@
-var path = require('path');
-var express = require('express');
 var webpack = require('webpack');
-var webpackMiddleware = require('webpack-dev-middleware');
-var config = require('./webpack.config.js');
+var webpackDevMiddleware = require('webpack-dev-middleware');
+var webpackHotMiddleware = require('webpack-hot-middleware');
+var config = require('./webpack.config');
+var cors = require('cors');
 
-const app = express();
-const compiler = webpack(config);
+var app = new (require('express'))();
+var port = 3000;
 
-app.use(express.static(__dirname + '/dist'));
+var compiler = webpack(config);
+app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
+app.use(webpackHotMiddleware(compiler));
 
-app.use(webpackMiddleware(compiler));
 
-
-app.get('*', function response(req, res) {
-  res.sendFile(path.join(__dirname, '/index.html'));
+app.use(function(req, res) {
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3000);
+
+
+
+// app.use(cors);
+
+app.listen(port, function(error) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port);
+  }
+});
