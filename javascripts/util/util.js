e408b55a22f738dcfa35f44e3bb1859929eac64c
Dan Cherouny <dcherouny@gmail.com>
2016-02-05 19:13:32 -0500

    Update proxy api call

--- javascripts/util/util.js
+++ javascripts/util/util.js
@@ -3,10 +3,10 @@ var $ = require('jquery');
 
 export let imageUrl = 'http://thecatapi.com/api/images/get?&format=xml&results_per_page=25';
 export let factUrl = 'http://catfacts-api.appspot.com/api/facts?number=25';
-let localhost = 'http://cherealnice.github.io/proxy?url=';
-
-imageUrl =  localhost + imageUrl;
-factUrl = localhost + factUrl;
+// let localhost = 'http://cherealnice.github.io/proxy?url=';
+//
+// imageUrl =  localhost + imageUrl;
+// factUrl = localhost + factUrl;
 
 export const parseCats = (images, facts) => {
   return $(images).find('image').map( (i, img) =>
