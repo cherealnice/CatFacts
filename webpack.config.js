5133d91919e483707fa159457bb11520733dc151
Dan Cherouny <dcherouny@gmail.com>
2016-02-05 19:00:26 -0500

    Fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   context: __dirname,
   entry: "./javascripts/cat_facts.js",
   output: {
-    path: path.join(__dirname, 'javascripts'),
+    path: ('./javascripts/'),
     filename: 'bundle.js',
     publicPath: '/static/'
   },
